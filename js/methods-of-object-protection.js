666b060b490bf552a02dab0abe5065be9249de57
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-07-24 12:49:38 +0300

    methods of object protection

---
+++ js/methods-of-object-protection.js
@@ -0,0 +1,42 @@
+const zoo = {
+    size: 40,
+    name: 'Central zoo',
+    owner: 'Bob',
+    animals: ['elaphant', 'monkey', 'wolf', 'pinguin'],
+};
+
+// заборона додавання
+Object.preventExtensions(zoo);
+
+zoo.size = 100;
+delete zoo.size;
+
+zoo.address = 'Kyiv'; // нова властивість
+
+console.log(zoo);
+console.log(Object.isExtensible(zoo));
+
+// заборона додавання та видалення
+Object.seal(zoo);
+
+zoo.address = 'Kyiv';
+delete zoo.owner;
+
+console.log(zoo);
+console.log(Object.isSealed(zoo));
+
+//заборона додаванняб видалення, зміни властивостей
+Object.freeze(zoo);
+
+zoo.address = 'Kyiv';
+delete zoo.owner;
+zoo.name = 'Noth zoo';
+
+console.log(zoo);
+console.log(Object.isFrozen(zoo));
+
+
+
+
+
+
